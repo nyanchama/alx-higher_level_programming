c347d69615272c3efe6707b6935cef6b592ebc88
Nyanchama Obara <92072653+nyanchama@users.noreply.github.com>
2022-06-16 03:54:51 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
-            print(name)
+    for function in dir(hidden_4):
+        if (function[0] != "_"):
+            print(function)
